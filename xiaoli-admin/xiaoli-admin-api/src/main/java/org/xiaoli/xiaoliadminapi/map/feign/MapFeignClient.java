77d5d70d48e9c1f7e24e405027f839ce872b0315
xiaoli79 <3099592185@qq.com>
2026-01-03 15:41:57 +0800

    实现地图相关的业务

--- xiaoli-admin/xiaoli-admin-api/src/main/java/org/xiaoli/xiaoliadminapi/map/feign/MapFeignClient.java
+++ xiaoli-admin/xiaoli-admin-api/src/main/java/org/xiaoli/xiaoliadminapi/map/feign/MapFeignClient.java
@@ -1,10 +1,24 @@
 package org.xiaoli.xiaoliadminapi.map.feign;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.xiaoli.xiaoliadminapi.map.domain.vo.RegionVO;
+import org.xiaoli.xiaolicommondomain.domain.R;
 
-@FeignClient
+import java.util.List;
+
+
+/**
+ * 区域信息VO
+ */
+@FeignClient(contextId = "mapFeignClient",value = "xiao-admin")
 public interface MapFeignClient {
 
+    /**
+     * 城市列表查询
+     * @return 城市列表信息
+     */
+    @GetMapping("/map/city_list")
+    R<List<RegionVO>> getCityList();
 
 }
