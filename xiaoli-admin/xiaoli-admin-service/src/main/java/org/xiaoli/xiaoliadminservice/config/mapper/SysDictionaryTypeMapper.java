0f2cd02e964118570ffac5bcfd36dc60721f7cd0
xiaoli79 <3099592185@qq.com>
2026-01-09 17:30:27 +0800

    实现字典相关的业务

---
+++ xiaoli-admin/xiaoli-admin-service/src/main/java/org/xiaoli/xiaoliadminservice/config/mapper/SysDictionaryTypeMapper.java
@@ -0,0 +1,19 @@
+package org.xiaoli.xiaoliadminservice.config.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * 字典类型表mapper接口~~
+ */
+@Mapper
+public interface SysDictionaryTypeMapper extends BaseMapper{
+
+
+
+
+
+
+}
