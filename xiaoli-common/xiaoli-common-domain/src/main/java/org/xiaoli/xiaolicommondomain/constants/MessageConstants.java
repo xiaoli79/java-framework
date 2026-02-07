41aa8da7e99b3ecbeb3695f88f62956380ac7d1d
xiaoli79 <3099592185@qq.com>
2026-02-07 16:15:12 +0800

    号码认证服务接口完成

--- xiaoli-common/xiaoli-common-domain/src/main/java/org/xiaoli/xiaolicommondomain/constants/MessageConstants.java
+++ xiaoli-common/xiaoli-common-domain/src/main/java/org/xiaoli/xiaolicommondomain/constants/MessageConstants.java
@@ -3,6 +3,11 @@ package org.xiaoli.xiaolicommondomain.constants;
 public class MessageConstants {
 
 
+    /**
+     * 发送短信的常量
+     */
+    public static final String PNS_MSG_OK = "OK";
 
-    public static final String SMS_MSG_OK = "OK";
+
+
 }
